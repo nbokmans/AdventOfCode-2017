64605cbc74164e6bb35d2a9c6c9c159a07848bd5
Niels Bokmans <niels@bluenotion.nl>
2017-12-02 22:06:41 +0100

    Loading puzzles

---
+++ Puzzle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode_2017
+{
+    public class Puzzle
+    {
+        private readonly int _day;
+        private readonly string _inputFileName;
+        private readonly bool _inputCached;
+
+        private const string SessionId = "53616c7465645f5f3408ba5c57b3f265a08a6396cfcb5a9ee83a3e0cd031b04e08a6dd5fc2ed2f203b4021a434437a35";
+
+        public Puzzle(int day)
+        {
+            _day = day;
+            _inputFileName = $"{Directory.GetCurrentDirectory()}/Day{_day}/input.txt";
+            _inputCached = File.Exists(_inputFileName);
+        }
+
+        public async Task<string> GetInput()
+        {
+            if (!_inputCached)
+            {
+                var request = WebRequest.Create($"http://adventofcode.com/2017/day/{_day}/input") as HttpWebRequest;
+                request.CookieContainer = new CookieContainer();
+                request.CookieContainer.Add(new Cookie("session", SessionId, "/", request.Host));
+                var url = $"http://adventofcode.com/2017/day/{_day}/input";
+                var response = await request.GetResponseAsync();
+                using (var fileStream = File.Create(_inputFileName))
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        var result = await reader.ReadToEndAsync();
+
+                        using (var writer = new StreamWriter(fileStream))
+                        {
+                            await writer.WriteLineAsync(result);
+                        }
+                    }
+                }
+            }
+
+            byte[] data;
+            using (var reader = File.Open(_inputFileName, FileMode.Open))
+            {
+                data = new byte[reader.Length];
+                await reader.ReadAsync(data, 0, (int)reader.Length);
+            }
+
+            return Encoding.ASCII.GetString(data);
+        }
+    }
+}
---
+++ Start.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode_2017
+{
+    public class Start
+    {
+        static async Task<string> GetPuzzleInput()
+        {
+            var puzzle = new Puzzle(1);
+            var input = await puzzle.GetInput();
+
+            return input;
+        }
+
+        static void Main(string[] args)
+        {
+            var x = Directory.GetCurrentDirectory();
+            var result = GetPuzzleInput().GetAwaiter().GetResult();
+            Console.WriteLine(result);
+            var y = 10;
+        }
+    }
+}
