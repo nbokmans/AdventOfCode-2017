5c496c3610a6097fc35bf96b1a7356385a7d1428
Niels Bokmans <niels@bluenotion.nl>
2017-12-05 08:58:44 +0100

    Day 5

---
+++ Day5/Day5Part1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode_2017.Day5
+{
+    public class Day5Part1 : Puzzle<int>
+    {
+        public Day5Part1() : base(5)
+        {
+        }
+
+        public override int Answer(string input)
+        {
+
+            var instructions = input.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToArray();
+            var index = 0;
+            var count = 0;
+            while (index >= 0 && index < instructions.Length)
+            {
+                count++;
+                var instruction = instructions[index];
+                instructions[index] += 1;
+                index += instruction;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Day5/Day5Part2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode_2017.Day5
+{
+    public class Day5Part2 : Puzzle<int>
+    {
+        public Day5Part2() : base(5)
+        {
+        }
+
+        public override int Answer(string input)
+        {
+
+            var instructions = input.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToArray();
+            var index = 0;
+            var count = 0;
+            while (index >= 0 && index < instructions.Length)
+            {
+                count++;
+                var instruction = instructions[index];
+                var offset = 1;
+                if (instruction >= 3)
+                {
+                    offset = -1;
+                }
+                instructions[index] += offset;
+                index += instruction;
+            }
+
+            return count;
+        }
+    }
+}
